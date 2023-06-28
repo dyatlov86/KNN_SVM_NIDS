1f54cde0e70970377de6e9b12bd8114d2c0a69e0
Onur Fırat Öztürk <37277494+dyatlov86@users.noreply.github.com>
2023-06-28 19:32:33 +0300

    Add files via upload

---
+++ Multi_Binary Karışık/cift_mdel.py
@@ -0,0 +1,94 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import f1_score,confusion_matrix,roc_auc_score,make_scorer,recall_score,precision_score,ConfusionMatrixDisplay
+
+packets = pd.read_csv('binary.csv')
+#X = data.iloc[:, :-1].values
+#y = data.iloc[:, -1].values
+packets.head()
+feature_names_packets = ['ip.flags.df','ip.flags.mf','ip.fragment','ip.fragment.count','ip.fragments','ip.ttl','ip.proto','tcp.window_size','tcp.ack','tcp.seq','tcp.len','tcp.stream','tcp.urgent_pointer','tcp.flags','tcp.analysis.ack_rtt','tcp.segments','tcp.reassembled.length','http.request','udp.port','frame.time_relative','frame.time_delta','tcp.time_relative','tcp.time_delta','tcp.port']
+X_packets = packets[feature_names_packets] #setting the col names
+y_packets = packets['label'] #setting the col names
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test=train_test_split(X_packets,y_packets,test_size=0.25)
+from sklearn.feature_selection import SelectKBest 
+from sklearn.feature_selection import mutual_info_classif,chi2,f_classif,SelectFpr,SelectPercentile
+select=SelectKBest(mutual_info_classif,k=8)
+select.fit(X_train,y_train)
+print(X_train.columns[select.get_support()])
+
+feature_names_packets=X_train.columns[select.get_support()]
+
+packets = pd.read_csv('binary.csv')
+#X = data.iloc[:, :-1].values
+#y = data.iloc[:, -1].values
+packets.head()
+
+X_packets = packets[feature_names_packets] #setting the col names
+y_packets = packets['label'] #setting the col names
+
+
+
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+X_train, X_test, y_train, y_test = train_test_split(X_packets, y_packets, test_size=0.2, random_state=42)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import cross_val_score,cross_val_predict
+import seaborn as sns
+
+
+knn = KNeighborsClassifier(n_neighbors=29)
+knn.fit(X_train, y_train)
+
+
+exploit = pd.read_csv('normal.csv')
+#X = data.iloc[:, :-1].values
+#y = data.iloc[:, -1].values
+packets.head()
+
+X_packets = exploit[['ip.ttl', 'tcp.window_size', 'tcp.ack', 'tcp.seq', 'tcp.stream','frame.time_relative', 'tcp.time_relative', 'tcp.port']] #setting the col names
+y_packets = exploit['label'] #setting the col names
+
+
+file1 = open('normal.csv', 'r')
+count = 0
+ 
+#while True:
+#    count += 1
+ 
+    # Get next line from file
+#    line = file1.readline()
+#    line=line.replace("\n","")
+#    ex=line.split(",")
+#    print(ex)
+x_new = scaler.transform(X_packets)
+y_pred = knn.predict(x_new)
+print(y_pred)
+anormal=0
+normal=0
+index=1
+for i in y_pred:
+    if i =="anormal":
+        anormal=anormal+1
+        print(index+1)
+    else:
+        normal=normal+1
+        #print(index+1)
+    index=index+1
+print("anormal:",anormal,"normal:",normal)
+    # if line is empty
+    # end of file is reached
+#    if not line:
+#        break
+    
+
+
+#print(f"Tahmin Edilen Sınıf: {y_pred[0]}")
